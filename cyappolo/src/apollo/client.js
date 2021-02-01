79fc400742e2c4ed355101adad61cff2056cd649
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-02-01 09:15:54 +0500

    added

--- cyappolo/src/apollo/client.js
+++ cyappolo/src/apollo/client.js
@@ -2,5 +2,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
   uri: "https://localhost:8888/.netlify/functions/graph_faunadb",
+  fetch,
   cache: new InMemoryCache(),
 })
