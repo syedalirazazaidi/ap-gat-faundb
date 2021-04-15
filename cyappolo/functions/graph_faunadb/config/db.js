4950d1d771d0bbc5e4dfe6e826168647389fce73
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-04-15 11:40:25 +0500

    remove file

--- cyappolo/functions/graph_faunadb/config/db.js
+++
@@ -1,8 +0,0 @@
-import faunadb from "faunadb"
-
-export default client = new faunadb.Client({
-  secret: process.env.FAUNA_SERVER_SECRET,
-})
-export default q = faunadb.query
-
-// export default { client, q }
